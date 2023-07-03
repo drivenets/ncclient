4668dd450ee1aa0775ecd03eb4b781c3412f0d06
muffi-dn <mpresswala@drivenets.com>
2023-07-03 12:20:55 +0100

    updated dnos drivers with namespace definitions

--- ncclient/devices/dnos.py
+++ ncclient/devices/dnos.py
@@ -1,4 +1,6 @@
 from .default import DefaultDeviceHandler
+from ncclient.xml_ import BASE_NS_1_0
+
 
 def passthrough(xml):
     return xml
@@ -13,4 +15,14 @@ class DnosDeviceHandler(DefaultDeviceHandler):
 
     def transform_reply(self):
         return passthrough
+    
+    def get_xml_base_namespace_dict(self):
+        return {None: BASE_NS_1_0}
 
+    def get_xml_extra_prefix_kwargs(self):
+        d = {}
+        d.update(self.get_xml_base_namespace_dict())
+        return {"nsmap": d}
+
+    def perform_qualify_check(self):
+        return False
